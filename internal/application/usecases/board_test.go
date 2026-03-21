2b8275adaf1144238f8b6995b75c71643861c624
vnicetn <vincentandpancakes@gmail.com>
2026-03-21 18:19:05 +0300

    unit testing

---
+++ internal/application/usecases/board_test.go
@@ -0,0 +1,47 @@
+package usecases
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/telekanban/internal/domain"
+)
+
+type mockRepo struct {
+	createCalled bool
+	lastBoard    *domain.Board
+}
+
+func (m *mockRepo) Create(ctx context.Context, board *domain.Board) error {
+	m.createCalled = true
+	m.lastBoard = board
+	return nil
+}
+
+func (m *mockRepo) FindByOwner(ctx context.Context, ownerID string) ([]*domain.Board, error) {
+	return nil, nil
+}
+
+func TestCreateBoard_Success(t *testing.T) {
+	repo := &mockRepo{}
+	uc := NewBoardUsecase(repo)
+
+	board, err := uc.CreateBoard(context.Background(), "My Board", "test_owner")
+
+	assert.NoError(t, err)
+	assert.NotNil(t, board)
+	assert.Equal(t, "My Board", board.Name)
+	assert.True(t, repo.createCalled)
+	assert.Equal(t, board, repo.lastBoard)
+}
+
+func TestCreateBoard_Invalid(t *testing.T) {
+	repo := &mockRepo{}
+	uc := NewBoardUsecase(repo)
+
+	board, err := uc.CreateBoard(context.Background(), "", "test_owner")
+
+	assert.Error(t, err)
+	assert.Nil(t, board)
+}
